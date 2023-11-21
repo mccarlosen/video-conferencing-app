3c7449b0eec395f0c335ce3e373d6bb2b279db2b
Carlos Meneses <osen.10112@gmail.com>
2023-11-21 17:30:23 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueCookies from 'vue-cookies'
 
 import App from './App.vue'
 import router from './router'
+import { registerService } from './services/register.service'
+import type { IRegisterService } from './types/register-service.interface'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-
+app.use(VueCookies)
+app.provide<IRegisterService>('RegisterService', registerService)
 app.mount('#app')
