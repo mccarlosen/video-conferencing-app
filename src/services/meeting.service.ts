3c3316d6cfeea72073a636cef2b5c69926f7bb2d
Carlos Meneses <osen.10112@gmail.com>
2023-11-22 17:32:38 -0500

    Create meeting.service.ts

---
+++ src/services/meeting.service.ts
@@ -0,0 +1,15 @@
+import type { MeetingConfig } from '@/types/meeting-config.type'
+import type { IMeetingService } from '@/services/meeting.interface'
+import axios, { type AxiosStatic } from 'axios'
+import type { VueCookies } from 'vue-cookies'
+
+class MeetingService implements IMeetingService {
+  BASE_URL: string = 'http://127.0.0.1:3333/api'
+  http: AxiosStatic = axios
+  
+  async create(data: MeetingConfig): Promise<any> {
+    return await this.http.post(`${this.BASE_URL}/create`, data)
+  }
+}
+
+export const meetingService = new MeetingService()
