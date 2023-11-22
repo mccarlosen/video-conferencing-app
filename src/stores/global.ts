8d8e91ec9ebd8a9fb741bc18d19a039468cd5928
Carlos Meneses <osen.10112@gmail.com>
2023-11-22 17:32:41 -0500

    Update global.ts

--- src/stores/global.ts
+++ src/stores/global.ts
@@ -1,29 +1,30 @@
 import { ref, type Ref, inject } from 'vue'
 import { defineStore } from 'pinia'
-import type { FormSecretKey } from '@/types/form-secret-key.type'
-import type { IRegisterService } from '@/types/register-service.interface'
-import type { VueCookies } from 'vue-cookies'
+import type { MeetingConfig } from '@/types/meeting-config.type'
+import type { IMeetingService } from '@/services/meeting.interface'
+// import type { VueCookies } from 'vue-cookies'
+import type { Meeting } from '@/types/types'
 
 export const useGlobalStore = defineStore('global', () => {
 
-  const $cookies: VueCookies | undefined = inject<VueCookies | undefined>('$cookies')
-  const registerService: IRegisterService | undefined = inject<IRegisterService>('RegisterService')
-  const authenticated: Ref<boolean> = ref(false)
-  const userName: Ref<string | undefined> = ref('')
-  const cookieName: Ref<string | undefined> = ref('')
-  // const roomName: Ref<string> = ref('')
+  // const $cookies: VueCookies | undefined = inject<VueCookies | undefined>('$cookies')
+  const meetingService: IMeetingService | undefined = inject<IMeetingService>('meetingService')
+  const meeting: Ref<Meeting | undefined> = ref(undefined)
 
-  async function registerWithSecretKey (form: FormSecretKey) {
+  async function createMeeting (config: MeetingConfig) {
     try {
-      const response = await registerService?.registerWithSecretKey(form)
-      const cookieData = await registerService?.registerCookie(response.data, $cookies)
-      authenticated.value = true
-      userName.value = cookieData?.username
-      cookieName.value = cookieData?.cookieName
-    } catch (e) {
-      console.log(e);
+      const response = await meetingService?.create(config)
+      const { data } = response
+      meeting.value = data
+      console.log(meeting.value); 
+      // await meetingService?.registerCookie(data, $cookies)
+    } catch (e: any) {
+      const { status, statusText } = e.response;
+      if (status == 404) {
+        console.log(statusText);
+      }
     }
   }
 
-  return { authenticated, userName, cookieName, registerWithSecretKey }
+  return { meeting, createMeeting }
 })
