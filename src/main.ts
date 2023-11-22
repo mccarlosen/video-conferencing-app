4519849f7ce8b349466f3e54395068d942f8896a
Carlos Meneses <osen.10112@gmail.com>
2023-11-22 17:32:36 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import VueCookies from 'vue-cookies'
 
 import App from './App.vue'
 import router from './router'
-import { registerService } from './services/register.service'
-import type { IRegisterService } from './types/register-service.interface'
+import { meetingService } from './services/meeting.service'
+import type { IMeetingService } from './services/meeting.interface'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueCookies)
-app.provide<IRegisterService>('RegisterService', registerService)
+app.provide<IMeetingService>('meetingService', meetingService)
 app.mount('#app')
