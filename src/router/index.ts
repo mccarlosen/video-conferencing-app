a82615be8f2abbe564d90f09f37d1fe69065b423
Carlos Meneses <osen.10112@gmail.com>
2023-11-23 01:54:44 -0500

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import RegisterView from '../views/RegisterView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: RegisterView
+      component: HomeView
     },
     {
       path: '/about',
