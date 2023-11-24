a2eefeaae35033478906dbf8b0e025f4e06e0ff6
Carlos Meneses <osen.10112@gmail.com>
2023-11-24 17:43:06 -0500

    Create VideoComponent.vue

---
+++ src/components/VideoComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{
+	muted?: boolean
+	stream?: any
+}>()
+</script>
+
+<template>
+	<video ref="videoTarget" :muted="props.muted" playsInline />
+</template>
