898f87db5f3a49030c3a133e66838b99c94e2441
Carlos Meneses <osen.10112@gmail.com>
2023-11-25 01:12:48 -0500

    Update VideoComponent.vue

--- src/components/VideoComponent.vue
+++ src/components/VideoComponent.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
 const props = defineProps<{
 	muted?: boolean
 	stream?: any
 }>()
+const videoTarget: any = ref(null)
+const playPromise = ref(Promise.resolve())
+const errorVideo = () => console.log('Enable to play video')
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+onMounted(() => {
+	const { stream } = props
+	if (stream && videoTarget.value && !videoTarget.value.srcObject) {
+		console.log(stream, videoTarget.value);
+		playPromise.value = playPromise.value.then(() => {
+			videoTarget.value.srcObject = stream;
+			return Promise.race([videoTarget.value.play().catch(errorVideo), sleep(150)]);
+		});
+	}
+})
+onBeforeUnmount(() => {
+	if (videoTarget.value) {
+		videoTarget.value.srcObject = null
+	}
+})
 </script>
 
 <template>
-	<video ref="videoTarget" :muted="props.muted" playsInline />
+	<video class="w-full rounded" ref="videoTarget" :muted="props.muted" playsInline />
 </template>
