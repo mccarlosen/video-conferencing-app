f2ac7e28a2d096ac5771a581c9982ab979f29d1d
Carlos Meneses <osen.10112@gmail.com>
2023-12-13 23:15:55 -0500

    Create ErrorMessage.vue

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useMeetingStore } from '@/stores/meeting';
+const meetingStore = useMeetingStore()
+</script>
+
+<template>
+  <template v-if="meetingStore.errorFormMessage">
+    <div class="rounded p-3 bg-red-500 text-white"><strong>Error:</strong> {{ meetingStore.errorFormMessage }}</div>
+  </template>
+</template>
