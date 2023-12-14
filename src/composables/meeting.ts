c05b303892b4ddef24a95fdba27f71bc1e02097e
Carlos Meneses <osen.10112@gmail.com>
2023-12-13 23:15:58 -0500

    Update meeting.ts

--- src/composables/meeting.ts
+++ src/composables/meeting.ts
@@ -1,26 +1,30 @@
 import type { MeetingConfig } from "@/types/meeting-config.type"
-import { ref, type Ref } from "vue"
 
 export function useMeetingService() {
 
-  const API_URL = import.meta.env.VITE_API_URL
+  const BASE_API_URL = import.meta.env.VITE_BASE_API_URL
   const EYESON_API_URL = import.meta.env.VITE_EYESON_API_URL
 
   async function create(data: MeetingConfig) : Promise<any> {
-    return fetch(`${API_URL}/create`, {
+    return fetch(`${BASE_API_URL}/create`, {
       method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(data)
     })
   }
 
-  function checkAccessKey(accessKey: string | string[]) {
-    const status: Ref<number | null> = ref(null)
-    fetch(`${EYESON_API_URL}/rooms/${accessKey}`)
-      .then((res) => (status.value = res.status))
-      .catch((err) => (console.log(err.message)))
-    return { status }
+  function checkAccessKey(accessKey: any) {
+    return fetch(`${EYESON_API_URL}/rooms/${accessKey}`)
   }
 
-  return { create, checkAccessKey }
+  function stopMeeting(accessKey: any) {
+    return fetch(`${EYESON_API_URL}/rooms/${accessKey}`, {
+      method: 'DELETE'
+    })
+  }
+
+  return { create, checkAccessKey, stopMeeting }
   
 }
