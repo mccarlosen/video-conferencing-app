e20ff1e5c54e0a4fa3de49bfb2f4ed32f5586ae3
Carlos Meneses <osen.10112@gmail.com>
2023-12-29 21:22:35 -0500

    Update FormCreateMeeting.vue

--- src/components/FormCreateMeeting.vue
+++ src/components/FormCreateMeeting.vue
@@ -41,13 +41,13 @@ const submitForm = async () => {
 </script>
 
 <template>
-  <form @submit.prevent="submitForm" class="space-y-5">
+  <form @submit.prevent="submitForm" class="space-y-3 sm:space-y-4 text-sm sm:text-base">
     <ErrorMessage />
     <div class="flex flex-col">
       <label for="name-for-meeting" class="mb-2">Your Name</label>
       <input v-model="userName" @blur="v$.userName.$touch" type="text" id="name-for-meeting" class="input-field mb-2" :class="{error: v$.userName.$error}"  placeholder="Example: John Wick">
-      <p class="text-sm font-light text-gray-600">This name will be displayed during the meeting.</p>
-      <p v-for="error of v$.userName.$errors" :key="error.$uid" class="text-sm font-light text-red-600" v-text="error.$message"></p>
+      <p class="text-xs sm:text-sm font-light text-gray-600">This name will be displayed during the meeting.</p>
+      <p v-for="error of v$.userName.$errors" :key="error.$uid" class="text-xs sm:text-sm font-light text-red-600" v-text="error.$message"></p>
     </div>
     <div class="flex flex-col">
       <label for="room-name" class="mb-2 inline-flex items-center">
@@ -57,7 +57,7 @@ const submitForm = async () => {
       <p v-for="error of v$.roomName.$errors" :key="error.$uid" class="text-sm font-light text-red-600" v-text="error.$message"></p>
     </div>
     <div class="flex justify-end">
-      <button class="rounded bg-c1-500 text-black transition duration-150 ease-in-out hover:bg-c1-400 px-4 py-2" v-text="sendingForm ? 'Processing' : 'Get Started'"></button>
+      <button class="rounded bg-c1-500 text-black transition duration-150 ease-in-out hover:bg-c1-400 px-4 py-2" v-text="sendingForm ? 'Processing' : 'Get Started'" :disabled="sendingForm"></button>
     </div>
   </form>
 </template>
