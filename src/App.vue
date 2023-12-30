b1b5544064899c5b6ccf51dd22e4b611b2b74f9b
Carlos Meneses <osen.10112@gmail.com>
2023-12-29 21:22:34 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TopBar from './components/TopBar.vue';
 <template>
 	<TopBar></TopBar>
 	<div class="w-full overflow-y-auto overflow-x-hidden pt-[70px]">
-		<div class="max-w-7xl mx-auto h-[calc(100vh_-70px)]">
+		<div class="max-w-6xl mx-auto h-[calc(100vh_-70px)]">
 			<router-view></router-view>
 		</div>
 	</div>
